e80def64b8e61d42988704fcff5267bae276bc0b
Rearte Gabriel <reartegabriel25@gmail.com>
2025-10-24 17:50:44 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Gestor_de_Libros',
+  webDir: 'www'
+};
+
+export default config;
